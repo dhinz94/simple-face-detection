61ca0ebb748830deeb32db3d2bebb0e62bc20f32
Dominic <dominic.hinz94@gmail.com>
2020-11-07 11:44:31 +0100

    show 10 images

--- visualize_dataset.py
+++ visualize_dataset.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 dataset_path='/content/drive/My Drive/celeba_copy/'
-image_path=dataset_path+'img_celeba'
+image_path=dataset_path+'img_celeba/'
 box_path=dataset_path+'list_bbox_celeba.txt'
 
 print('image_path exists: ',os.path.exists(image_path))
@@ -20,6 +20,16 @@ file_list=[x for x in file_list if '.jpg' in x]
 boxes=boxes[boxes['image_id'].isin(file_list)]
 
 print(boxes)
+
+
+for i in range(10):
+    num=np.random.randint(0,len(boxes))
+    file_path=image_path+file_list[num]
+
+    image=plt.imread(file_path)
+    plt.imshow(image)
+    plt.show()
+
 
 
 
