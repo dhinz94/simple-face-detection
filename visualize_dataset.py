7562c52c3d18ea6ac96025af0cbd9dbf3dbf426d
Dominic <dominic.hinz94@gmail.com>
2020-11-07 10:28:58 +0100

    visualize bounding box dataframe

--- visualize_dataset.py
+++ visualize_dataset.py
@@ -1,9 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import pandas as pd
 
-dataset_path='/content/drive/'
 
-print('data_path exists: ',os.path.exists(dataset_path))
+dataset_path='/content/drive/My Drive/celeba_copy/'
+image_path=dataset_path+'img_celeba'
+box_path=dataset_path+'list_bbox_celeba.txt'
+
+print('image_path exists: ',os.path.exists(image_path))
+print('box_path exists: ',os.path.exists(box_path))
+
+boxes=pd.read_csv(box_path,sep=' ')
+print(boxes)
+
 
 
