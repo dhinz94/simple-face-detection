23fe19ca79b9c4bbb15a2a9f432f73ff38047d4e
Dominic <dominic.hinz94@gmail.com>
2020-11-08 13:01:40 +0100

    update

--- train_model.py
+++ train_model.py
@@ -50,7 +50,7 @@ def block(input,filter_amount):
 dataset_path = '/content/drive/My Drive/simple_face_detection_data/'
 
 # local path for development
-dataset_path='/home/dominic/Dokumente/Github/simple-face-detection/data/'
+# dataset_path='/home/dominic/Dokumente/Github/simple-face-detection/data/'
 
 activation = tf.nn.relu
 normalization = BatchNormalization
